7f22414018a3c21735f73c530b5574cc9639df9d
Kalina Dukova <kalina984@abv.bg>
2022-02-14 10:01:03 +0200

    Sixth task

--- 06.Blog/app.js
+++ 06.Blog/app.js
@@ -1,5 +1,69 @@
 function attachEvents() {
-    console.log('TODO...');
+    const loadPostBtn = document.getElementById('btnLoadPosts');
+    const viewPostBtn = document.getElementById('btnViewPost');
+
+    loadPostBtn.addEventListener('click', loadPost);
+    viewPostBtn.addEventListener('click', viewPost);
+}
+
+async function loadPost() {
+    const menu = document.getElementById('posts');
+    try {
+        const res = await fetch('http://localhost:3030/jsonstore/blog/posts');
+        if (res.status != 200) {
+            throw new Error(`${res.status} ${res.statusText}`);
+        }
+        const data = await res.json();
+        const dataKeys = Object.keys(data);
+
+        menu.replaceChildren();
+        
+        for (const id of dataKeys) {
+            const option = document.createElement('option');
+            option.value = id;
+            option.textContent = data[id].title;
+            menu.appendChild(option);
+        }
+        
+    } catch (error) {
+        menu.textContent = error.message;
+    }
+}
+
+async function viewPost() {
+    const id = document.getElementById('posts').value;
+    const commentsBody = document.getElementById('post-comments');
+    const post = await getPostbyId(id);
+
+    commentsBody.replaceChildren();
+
+    document.getElementById('post-title').textContent = post.title;
+    document.getElementById('post-body').textContent = post.body;
+
+    const comments = await getCommentsById(id);
+
+    comments.forEach(comment => {
+        const liElement = document.createElement('li');
+        liElement.id = comment.id;
+        liElement.textContent = comment.text;
+
+        commentsBody.appendChild(liElement);
+    });
+}
+
+async function getPostbyId(id) {
+    const res = await fetch(`http://localhost:3030/jsonstore/blog/posts/${id}`);
+    const data = await res.json();
+
+    return data;
+}
+
+async function getCommentsById(id) {
+    const res = await fetch('http://localhost:3030/jsonstore/blog/comments');
+    const data = await res.json();
+
+    const filteredComments = Object.values(data).filter(comment => comment.postId === id);
+    return filteredComments;
 }
 
 attachEvents();
